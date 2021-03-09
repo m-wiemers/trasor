6f5d15f86c8d56734224c1b0e2dee19261c98bfd
m-wiemers <marcel@klebauter.de>
2021-03-09 18:05:21 +0100

    add client

---
+++ client/pages/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [passwordName, setPasswordName] = useState("");
+  const [passwordDoc, setPasswordDoc] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const result = await fetch(
+      `http://localhost:3333/api/passwords/${passwordName}`
+    );
+    const passwordDoc = await result.json();
+    setPasswordDoc(passwordDoc);
+  }
+
+  return (
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          value={passwordName}
+          onChange={(event) => setPasswordName(event.target.value)}
+        />
+        <button type="submit">send</button>
+        <p></p>
+      </form>
+      {passwordDoc && (
+        <>
+          {passwordDoc.name} {passwordDoc.value}
+        </>
+      )}
+    </>
+  );
+}
